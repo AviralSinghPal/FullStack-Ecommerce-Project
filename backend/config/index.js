f97d2e51bc75242932aed6ee4042a23b63b309ac
AviralSinghPal <aviralpal00007@gmail.com>
2022-12-10 00:05:03 +0530

    added a mail utility and forgot password

--- backend/config/index.js
+++ backend/config/index.js
@@ -7,6 +7,10 @@ const config = {
     JWT_EXPIRY: process.env.JWT_EXPIRY || "30d",
     MONGODB_URL: process.env.MONGODB_URL,
     PORT: process.env.PORT,
+    SMTP_MAIL_HOST: valueone,
+    SMTP_MAIL_PORT: valuetwo,
+    SMTP_MAIL_USERNAME: valuethree,
+    SMTP_MAIL_PASSWORD: valuefour
 }
 
 export default config
