5692f558bc6a73fefee48d1e4c3972730ee5e5a3
AviralSinghPal <aviralpal00007@gmail.com>
2022-12-09 19:21:09 +0530

    modified product schema

--- backend/models/product.schema.js
+++ backend/models/product.schema.js
@@ -4,6 +4,40 @@ import mongoose from "mongoose";
     {
         name:{
             type:String,
+            required: [true, "Please provide a product name"],
+            trim: true,
+            maxLength: [120, "Product name should be a max  of 120 characters"]
+        },
+        price:{
+            type: Number,
+            required: [true, "Please provide a product price"],
+            trim: true,
+            maxLength: [6, "Product name should be less than 1000000"]
+        },
+        description: {
+            type: String,
+            //use some kind od editor (Assignment)
+        },
+        photos: [
+            {
+                secure_url: {
+                    type: String,
+                    required: true
+                }
+            },
+        ],
+        stock: {
+            type: Number,
+            default: 0
+        },
+        collectionId: {
+            type: mongoose.Schema.Types.ObjectId ,
+            ref: "Collection"
         }
+    },
+    {
+        timestamps: true
     }
  )
+
+ export default mongoose.model("Product", productSchema)
