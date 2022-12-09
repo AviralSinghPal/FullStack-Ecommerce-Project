51f61ac46974c1838c4ea447c926ee21b01a0922
AviralSinghPal <aviralpal00007@gmail.com>
2022-12-09 22:32:21 +0530

    added morgan express cors  middle ware

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,16 @@ import  config  from "./config/index"
 (async () => {
     try {
         await mongoose.connect(config.MONGODB_URL)
-        console.log("DB CONNECTED")
+        console.log("DB CONNECTED");
+
+        app.on('error', (err) => {
+            console.log("error: " , err);
+        })
+        const onListening = () =>{
+            console.log(`Listening on ${config.PORT}`);
+        }
+
+        app.listen(config.PORT, onListening)
     } catch (err) {
         console.log("ERROR ", err );
         throw err
